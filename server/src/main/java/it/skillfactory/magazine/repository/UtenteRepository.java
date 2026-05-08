5e45ba4b523ba511fb231ff88cf072b8ca3d725c
Mirko Onorato <85618159+mirkoSkF@users.noreply.github.com>
2026-05-08 14:31:10 +0200

    Delete server directory

--- server/src/main/java/it/skillfactory/magazine/repository/UtenteRepository.java
+++
@@ -1,9 +0,0 @@
-package it.skillfactory.magazine.repository;
-
-import it.skillfactory.magazine.model.Utente;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UtenteRepository extends JpaRepository<Utente, Long> {
-    Optional<Utente> findByUsername(String username);
-}
