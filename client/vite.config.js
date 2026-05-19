2460ec9fed996cb11c6dd3a76f58f5376f1688c6
Mirko Onorato <85618159+mirkoSkF@users.noreply.github.com>
2026-05-19 18:16:09 +0200

    Add files via upload

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: true,
+    hmr: false,
+    proxy: {
+      '/api': {
+        target: 'http://sf-magazine-backend:8096',
+        changeOrigin: true,
+        secure: false,
+        // 👇 AGGIUNGI QUESTO BLOCCO QUI SOTTO
+        configure: (proxy, _options) => {
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            // Sovrascrive l'Origin della richiesta verso il backend, 
+            // facendogli credere che arrivi da localhost per superare il filtro Java
+            proxyReq.setHeader('Origin', 'http://localhost:5173');
+          });
+        }
+      }
+    }
+  }
 })
